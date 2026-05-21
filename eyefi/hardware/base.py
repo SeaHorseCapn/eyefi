14d6dca72c4fc68e57e84d55253140ffb0e7a11a
SeaHorseCapn <newwavecap1@proton.me>
2026-05-20 20:18:21 -0700

    Add hardware/base.py

---
+++ eyefi/hardware/base.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+"""
+eyefi.hardware.base
+
+Copyright (c) 2026 eyefi Contributors
+SPDX-License-Identifier: MIT
+"""
+
+from abc import ABC
+
+class RadioSensor(ABC):
+    pass
