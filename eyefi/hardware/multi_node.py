c87723405fa64521d4badb49ff267f8ccbe7ce64
SeaHorseCapn <newwavecap1@proton.me>
2026-05-20 20:30:22 -0700

    Add multi_node.py

---
+++ eyefi/hardware/multi_node.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+"""
+eyefi.hardware.multi_node
+
+Copyright (c) 2026 eyefi Contributors
+SPDX-License-Identifier: MIT
+"""
+
+class MultiNodeManager:
+    pass
